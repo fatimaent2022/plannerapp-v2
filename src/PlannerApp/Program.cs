0ae004e0b4f3b7560cd9534be03ef455c2633b04
fatima ent <fatimaent2022@gmail.com>
2022-03-25 00:04:23 +0500

    before push to github.

--- src/PlannerApp/Program.cs
+++ src/PlannerApp/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -7,6 +8,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddMudServices();
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+////Added By Us
+builder.Services.AddHttpClient("PlannerApp.Api", client =>
+{
+    client.BaseAddress = new Uri("https://plannerapp-api.azurewebsites.net");
+}).AddHttpMessageHandler<AuthorizationMessageHandler>();
+
+builder.Services.AddTransient<AuthorizationMessageHandler>();
+
+builder.Services.AddMudServices();//Added By Us
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddScoped(sp => sp.GetService<IHttpClientFactory>().CreateClient("PlannerApp.Api"));
+
 await builder.Build().RunAsync();
