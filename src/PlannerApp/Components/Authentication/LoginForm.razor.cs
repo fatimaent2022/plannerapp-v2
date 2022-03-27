91ce5b8d6215bb66e7a379cff71342dbd3a7e07c
fatimaent2022 <102145142+fatimaent2022@users.noreply.github.com>
2022-03-27 15:53:02 +0500

    Login Button Functionality

--- src/PlannerApp/Components/Authentication/LoginForm.razor.cs
+++ src/PlannerApp/Components/Authentication/LoginForm.razor.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using PlanerApp.Shared.Models;
+using PlanerApp.Shared.Responses;
+using System.Net.Http.Json;
 
 namespace PlannerApp.Components
 {
@@ -7,10 +11,50 @@ namespace PlannerApp.Components
     {
         [Inject]
         public HttpClient HttpClient { get; set; }
+
+        [Inject]
+        public NavigationManager Navigation { get; set; }
+
+        [Inject]
+        public AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        public ILocalStorageService Storage { get; set; }
+
+
         private LoginRequest _model = new LoginRequest();
+
+        private bool _isBusy = false;
+        private string _errorMessage = string.Empty;
+
         private async Task LoginUserAsync()
-        { 
-        throw new NotImplementedException();
+        {
+            _isBusy = true;
+            _errorMessage = string.Empty;
+
+            var response = await HttpClient.PostAsJsonAsync("/api/v2/auth/login",_model);
+
+            if (response.IsSuccessStatusCode)
+            { 
+            var result=await response.Content.ReadFromJsonAsync<ApiResponse<LoginResult>>();
+
+                //Store Token in Local Storage
+
+                await Storage.SetItemAsStringAsync("access_token", result.Value.Token);
+                await Storage.SetItemAsync("expiry_date", result.Value.ExpiryDate);
+
+                //Notify New Change to other components, 
+                await AuthenticationStateProvider.GetAuthenticationStateAsync();
+
+                Navigation.NavigateTo("/");
+            }
+
+            else
+            {
+                var errorResult = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+                _errorMessage = errorResult.Message;
+            }
+            _isBusy = false;
         }
 
     }
