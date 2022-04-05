831a68be181299e7b2ca7287fee0b7f9c22acc44
fatimaent2022 <102145142+fatimaent2022@users.noreply.github.com>
2022-04-06 01:17:33 +0500

    search bar and pagination

--- src/PlannerApp/Components/Plans/PlanCardsList.razor.cs
+++ src/PlannerApp/Components/Plans/PlanCardsList.razor.cs
@@ -5,7 +5,7 @@ namespace PlannerApp.Components
 {
     public partial class PlanCardsList
     {
-      private bool _isBusy { get; set; }
+        private bool _isBusy { get; set; }
         private int _pageNumber = 1;
         private int _pageSize = 10;
         private string _query = string.Empty;
@@ -13,11 +13,18 @@ namespace PlannerApp.Components
         [Parameter]
         public Func<string, int, int, Task<PagedList<PlansSummary>>> FetchPlans { get; set; }
 
-        private PagedList<PlansSummary> _result=new();
+        private PagedList<PlansSummary> _result = new();
         protected override async Task OnInitializedAsync()
         {
+            await GetPlansAsync(1);
+        }
+
+        private async Task GetPlansAsync(int pageNumber = 1)
+        {
+            _pageNumber = pageNumber;
             _isBusy = true;
-            _result = await FetchPlans?.Invoke(_query,_pageNumber,_pageSize);
+
+            _result = await FetchPlans?.Invoke(_query, _pageNumber, _pageSize);
 
             _isBusy = false;
         }
